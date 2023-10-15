9ca54a864dfd30ef7afbb93ae1e144a45bdf4e24
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-10-14 23:11:06 -0400

    changed workflow for files

---
+++ src/app/processor/components/processorFunctions.js
@@ -0,0 +1,17 @@
+import Papa from "papaparse";
+
+export function initUpload(file) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      Papa.parse(file, {
+        header: true,
+        complete: function (results) {
+          const transactions = results.data;
+          resolve(transactions);
+        },
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
--- src/app/processor/page.js
+++ src/app/processor/page.js
@@ -1,154 +1,47 @@
 "use client";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import CloudUploadIcon from "@mui/icons-material/CloudUpload";
-import styled from "@emotion/styled";
-
 import { useEffect, useState } from "react";
-import Image from "next/image";
 
-import CustomizedSnackbars from "../components/flashMsg";
-import { apiCall } from "../components/api/api";
-
-const VisuallyHiddenInput = styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
-  height: 1,
-  overflow: "hidden",
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  whiteSpace: "nowrap",
-  width: 1,
-});
+import Home from "./components/home";
+import { initUpload } from "./components/processorFunctions";
 
 export default function Processor() {
   const [file, setFile] = useState(null);
   const [openMsg, setOpenMsg] = useState(false);
   const [msgContent, setMsgContent] = useState({});
   const [data, setData] = useState(null);
+  const [formStep, setFormStep] = useState(0);
 
   useEffect(() => {
     if (file) {
-      console.log("running fn");
-      const formData = new FormData();
-      formData.append("csv_file", file);
-
-      console.log("getting data");
       const fetchData = async () => {
         try {
-          const jsonFromCsv = await apiCall(
-            "get",
-            "http://localhost:8000/api/crsf/",
-            formData
-          );
-          setData(jsonFromCsv);
-          console.log("data set", jsonFromCsv);
+          const transactions = await initUpload(file);
+          setData(transactions);
+          console.log("data set", transactions);
         } catch (error) {
           console.log("error:", error);
         }
       };
-      if (data == null) {
-        fetchData();
-      }
+
+      fetchData();
     }
   }, [file]);
 
-  const handleFileChange = (e) => {
-    const file = e.target.files[0];
-
-    if (file && file.name.endsWith(".csv")) {
-      setFile(file);
-      setMsgContent({ type: "success", msg: "CSV uploaded successfully!" });
-    } else {
-      setMsgContent({ type: "error", msg: "Please select a valid .csv file." });
-    }
-    setOpenMsg(true);
-  };
-
-  return (
-    <Box
-      sx={{
-        flexGrow: 1,
-        padding: 4,
-        backgroundColor: "#242424",
-        minHeight: "100vh",
-        display: "flex",
-      }}
-    >
-      {/* <Grid container spacing={1}>
-        <Grid
-          item
-          xs={6}
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            marginTop: "60px",
-          }}
-        >
-          <Typography variant="h2">
-            Welcome to Banksy, your analytical finance tool.
-          </Typography>
-          <Typography
-            variant="h6"
-            sx={{ marginTop: "10px", marginBottom: "20px" }}
-          >
-            Upload a .csv file to get started.
-          </Typography>
-          <Button
-            component="label"
-            variant="contained"
-            startIcon={<CloudUploadIcon />}
-            sx={{ width: "200px", height: "50px" }}
-          >
-            Upload file
-            <VisuallyHiddenInput type="file" onChange={handleFileChange} />
-          </Button>
-          <Box
-            sx={{
-              display: "flex",
-              marginTop: "20px",
-            }}
-          >
-            {file ? (
-              <>
-                <p>selected file: {file.name}</p>
-                <Button
-                  component="label"
-                  variant="contained"
-                  sx={{
-                    backgroundColor: "#4caf50",
-                    "&:hover": {
-                      backgroundColor: "#4caf50",
-                    },
-                    margin: "-7px 0 0 15px",
-                  }}
-                >
-                  Start
-                </Button>
-              </>
-            ) : (
-              ""
-            )}
-          </Box>
-        </Grid>
-        <Grid item xs={6} sx={{ marginTop: "-40px" }}>
-          <Image
-            src="/modern1.png"
-            alt="Description of the image"
-            width={600}
-            height={600}
-            priority
-          />
-        </Grid>
-      </Grid> */}
-      <CustomizedSnackbars
-        open={openMsg}
-        setOpen={setOpenMsg}
+  if (formStep === 0) {
+    return (
+      <Home
+        data={data}
+        setData={setData}
+        setOpenMsg={setOpenMsg}
+        openMsg={openMsg}
+        file={file}
+        setFile={setFile}
         msgContent={msgContent}
+        setMsgContent={setMsgContent}
+        setFormStep={setFormStep}
       />
-    </Box>
-  );
+    );
+  } else {
+    return <h1>next step</h1>;
+  }
 }
