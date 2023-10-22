6f887df179508031bffba51e35492c2f64edf573
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-10-22 15:42:01 -0400

    messing around with signatures

--- src/app/components/globalContext/index.js
+++ src/app/components/globalContext/index.js
@@ -6,9 +6,16 @@ const GlobalContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [msg, setMsg] = useState([]);
 
-  const addMsg = useCallback((type, msgContent) => {
-    setMsg((prevArr) => [...prevArr, { type, msg: msgContent }]);
-  });
+  const addMsg = useCallback(
+    /**
+     * This is a template for setting up the signature
+     * @param {String} type
+     * @param {String} msgContent
+     */
+    (type, msgContent) => {
+      setMsg((prevArr) => [...prevArr, { type, msg: msgContent }]);
+    }
+  );
 
   return (
     <GlobalContext.Provider value={{ user, setUser, msg, setMsg, addMsg }}>
