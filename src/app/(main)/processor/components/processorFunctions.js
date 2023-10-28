d856f580788308041a0056375ae769c2440b25be
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-10-28 02:27:22 -0400

    cleared out old processing function

--- src/app/(main)/processor/components/processorFunctions.js
+++ src/app/(main)/processor/components/processorFunctions.js
@@ -17,34 +17,3 @@ export function parseRawFile(file) {
     }
   });
 }
-
-export function assignCategories(data, categoryKeys) {
-  try {
-    console.log("assigning categories");
-    for (const transaction of data) {
-      const desc = transaction.Description.toLowerCase();
-
-      for (const categoryName in categoryKeys) {
-        const match = categoryKeys[categoryName].keys.find((key) =>
-          desc.includes(key)
-        );
-
-        if (match) {
-          transaction["Category"] = categoryKeys[categoryName].ref;
-          break;
-        }
-
-        transaction["Category"] = categoryKeys["flag"].ref;
-      }
-    }
-
-    const retObj = {
-      good: data.filter((t) => t.Category !== 400),
-      bad: data.filter((t) => t.Category === 400),
-    };
-
-    return retObj;
-  } catch (error) {
-    throw error;
-  }
-}
