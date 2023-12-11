b6b4cf4265c9d10f883a363dd45ded634477c798
johngaynor <86327580+johngaynor@users.noreply.github.com>
2023-12-10 21:20:19 -0500

    cleaned up various things

--- src/app/(main)/history/page.js
+++ src/app/(main)/history/page.js
@@ -41,7 +41,7 @@ export default function History() {
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [showPercents, setShowPercents] = useState(false);
   const [openReport, setOpenReport] = useState(false);
-  const [comparePeriod, setComparePeriod] = useState(1);
+  const [comparePeriod, setComparePeriod] = useState(3);
 
   useEffect(() => {
     if (user && !userHistory && !historyLoading) {
@@ -85,37 +85,40 @@ export default function History() {
     let prevSpending = 0;
     let prevSavings = 0;
 
+    let validRows = 0;
+
     for (let i = 1; i <= comparePeriod; i++) {
       const row = filteredHistory[currentRow.index + i];
       if (row) {
+        validRows += 1;
         prevIncome += row.income;
         prevSpending += row.spending;
         prevSavings += row.income - row.spending;
       }
     }
 
-    prevIncome = prevIncome / comparePeriod;
-    prevSpending = prevSpending / comparePeriod;
-    prevSavings = prevSavings / comparePeriod;
+    prevIncome = validRows ? prevIncome / validRows : prevIncome;
+    prevSpending = validRows ? prevSpending / validRows : prevSpending;
+    prevSavings = validRows ? prevSavings / validRows : prevSavings;
 
-    // console.log(
-    //   "current row:",
-    //   filteredHistory[currentRow.index].month_year,
-    //   "stats for previous row:",
-    //   prevIncome
-    // );
-
-    const income = showPercents
-      ? ((currentRow.income - prevIncome) / prevIncome) * 100
-      : currentRow.income - prevIncome;
-    const spending = showPercents
-      ? ((currentRow.spending - prevSpending) / prevSpending) * 100
-      : currentRow.spending - prevSpending;
-    const savings = showPercents
-      ? ((currentRow.savings - prevSavings) / prevSavings) * 100
-      : currentRow.savings - prevSavings;
+    const income = validRows
+      ? showPercents
+        ? ((currentRow.income - prevIncome) / prevIncome) * 100
+        : currentRow.income - prevIncome
+      : 0;
+    const spending = validRows
+      ? showPercents
+        ? ((currentRow.spending - prevSpending) / prevSpending) * 100
+        : currentRow.spending - prevSpending
+      : 0;
+    const savings = validRows
+      ? showPercents
+        ? ((currentRow.savings - prevSavings) / prevSavings) * 100
+        : currentRow.savings - prevSavings
+      : 0;
 
     return { income, spending, savings };
+    // return { prevIncome, prevSpending, prevSavings, income, spending, savings };
   };
 
   const viewReport = (report) => {
@@ -254,6 +257,21 @@ export default function History() {
                     .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                     .map((row, index) => {
                       const comparativeStats = generateComparativePeriod(row);
+                      // const {
+                      //   prevIncome,
+                      //   prevSpending,
+                      //   prevSavings,
+                      //   income,
+                      //   spending,
+                      //   savings,
+                      // } = comparativeStats;
+                      // console.log(
+                      //   row.index,
+                      //   "prev",
+                      //   prevIncome,
+                      //   "calc",
+                      //   income
+                      // );
                       return (
                         <TableRow
                           key={index}
